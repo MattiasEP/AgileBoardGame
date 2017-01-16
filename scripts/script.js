fee11f4cc427c95923fc8c959f372b31c43a1e54
Mattias Söderström <mattias.ep@gmail.com>
2017-01-16 11:23:21 +0100

    Design (mostly)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
 $('.us').draggable();
+
+
+var AgileBoardGame = {
+
+    init: function() {
+        this.cacheDom();
+        this.bindEvents();
+    },
+
+    cacheDom: function() {
+        console.log(this.dice());
+    },
+
+    bindEvents: function() {
+
+    },
+
+    dice: function(amount) {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+
+}
+
+AgileBoardGame.init();
