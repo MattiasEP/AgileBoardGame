6a6303323731e4e9699ab53f1800ad47ff43fb6e
puttetham <patrikhogler@hotmail.com>
2017-02-15 12:53:03 +0100

    installera webpack för att få det att funka
    -

--- Admin/userstory_admin.php
+++ Admin/userstory_admin.php
@@ -1,12 +1,13 @@
 <div id="addUserStory">
-	<form action="add_us.php" method="post" name="us">
-		Type of card: <input type="text" name="type" /><br>
-		Name of card: <input type="text" name="name" /><br>
-        Value: <input type="text" name="value" /><br>
-        Analysis: <input type="text" name="analysis" /><br>
-        Developing: <input type="text" name="develop" /><br>
-        Testing: <input type="text" name="testing" /><br>
 
+	<form action="add_us.php" method="post" name="us">
+			Type of card: <input type="text" name="type" /><br>
+			Name of card: <input type="text" name="name" /><br>
+			Value: <input type="text" name="value" /><br>
+			Analysis: <input type="text" name="analysis" /><br>
+			Developing: <input type="text" name="develop" /><br>
+			Testing: <input type="text" name="testing" /><br>
  		<input type="submit" value="Lägg till kort" />
  	</form>
+	
 </div>
