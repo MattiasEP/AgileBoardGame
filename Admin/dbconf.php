dadbdfba0754155cee418750bb1249926f9bc0cd
Robert Horngren <robert.horngren@student.kyh.se>
2017-03-01 16:47:53 +0100

    Card admin
    Tokig blir man... Testar att synka upp... igen!

---
+++ Admin/dbconf.php
@@ -0,0 +1,5 @@
+<?php	
+    //$db = mysqli_connect('server', 'username', 'password', 'database');
+	$db = mysqli_connect('localhost', 'root', 'root', 'boardgame');
+	mysqli_query($db, 'SET NAMES utf8');
+?>
